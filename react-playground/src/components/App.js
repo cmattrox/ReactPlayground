170af1aceaf801c312a56c4f7e439bac77c7160c
Charlie Welch <welch.charlie16@gmail.com>
2021-11-29 15:33:12 -0600

    building header

--- react-playground/src/components/App.js
+++ react-playground/src/components/App.js
@@ -1,11 +1,20 @@
 import './App.css';
 
-function App() {
+function HeaderBar() {
   return (
-    <div className="App" >
-      <h1 className="border-blue-500 border-2 ">Hello World!</h1>
+    <div className="HeaderBar" >
+      <nav className="bg-gray-900">
+        <div className="px-10 py-5 flex flex-row w-full">
+          <h1 className="text-white text-2xl border-white border-2 w-1/3">Charles Welch</h1>
+          <div className="Relative w-2/3">
+              <a className="text-white border-2 border-white px-5 absolute right-40">Experience</a>
+              <a className="text-white border-2 border-white px-5 absolute right-20">Projects</a>
+              <a className="text-white border-2 border-white px-5 absolute right-0">Contact</a>
+          </div>
+        </div>
+      </nav>
     </div>
   );
 }
 
-export default App;
+export default HeaderBar;
